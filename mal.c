5a8a5d9136e7687811c1b42c6854ce307f795bb9
Deyzer2566 <andrew.kozodoy@yandex.ru>
2025-05-07 21:33:42 +0300

    восстановление значения хранилища, если на момент восстановления значение идентификатора(если есть) поменялось

--- mal.c
+++ mal.c
@@ -2,39 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-static int storage_access[STORAGE_COUNT]={0};
-storage_t get_least_used_register() {
-    int max_access = 0;
-    storage_t reg_num = 0;
-    for(int i = 0;i < STORAGE_COUNT; i++) {
-        if(storage_access[i] > max_access) {
-            max_access = storage_access[i];
-            reg_num = i;
-        }
-        storage_access[i] ++;
-    }
-    storage_access[reg_num] = (signed)-3;
-    reg_num += r1;
-    return reg_num;
-}
-
-storage_t get_storage(FILE *fd) {
-    storage_t least_used = get_least_used_register();
-    addi_oper_backend(fd, sp, sp, (sword_t)-WORD_SIZE);
-    save_oper_backend(fd, least_used, sp, 0);
-    return least_used;
-}
-
-void free_storage(FILE *fd, storage_t storage) {
-    load_oper_backend(fd, storage, sp, 0);
-    addi_oper_backend(fd, sp, sp, WORD_SIZE);
-}
-
-void allocate_storage(storage_t storage) {
-    if(storage != zero)
-        storage_access[storage - r1] = 0;
-}
-
 struct ident_t {
     char* name;
     enum {
@@ -56,6 +23,76 @@ struct ident_list_t {
     struct list_header_t *next;
     struct ident_t ident;
 };
+
+storage_t get_least_used_register();
+storage_t get_storage(FILE *fd);
+void free_storage(FILE *fd, storage_t storage);
+void allocate_storage(storage_t storage);
+struct ident_t *find_ident_by_name(char *name);
+struct ident_t *find_ident_by_reg(storage_t storage);
+int load_ident(FILE *fd, storage_t dest, char* name, bool create_if_not_exists);
+void save_ident(FILE *fd, struct ident_t *ident);
+void inc_idents_addresses();
+void dec_idents_addresses();
+
+static int storage_access[STORAGE_COUNT]={0};
+storage_t get_least_used_register() {
+    int max_access = 0;
+    storage_t reg_num = 0;
+    for(int i = 0;i < STORAGE_COUNT; i++) {
+        if(storage_access[i] > max_access) {
+            max_access = storage_access[i];
+            reg_num = i;
+        }
+        storage_access[i] ++;
+    }
+    storage_access[reg_num] = (signed)-3;
+    reg_num += r1;
+    return reg_num;
+}
+
+struct register_ident_mapping_list_t {
+    struct list_header_t *next;
+    struct ident_t *ident;
+};
+struct register_ident_mapping_list_t *mapping_history[STORAGE_COUNT] = {NULL};
+void push_in_mapping_history(storage_t storage, struct ident_t *ident) {
+    struct register_ident_mapping_list_t *new_ident = malloc(sizeof(struct register_ident_mapping_list_t *));
+    new_ident->next = (struct list_header_t *)mapping_history[storage-r1];
+    new_ident->ident = ident;
+    mapping_history[storage-r1] = new_ident;
+}
+void pop_from_mapping_history(FILE* fd, storage_t storage) {
+    struct register_ident_mapping_list_t *ident = mapping_history[storage-r1];
+    if(ident->ident != NULL)
+        load_ident(fd, storage, ident->ident->name, false);
+    mapping_history[storage-r1] = (struct register_ident_mapping_list_t *)ident->next;
+    free(ident);
+}
+storage_t get_storage(FILE *fd) {
+    storage_t least_used = get_least_used_register();
+    addi_oper_backend(fd, sp, sp, (sword_t)-WORD_SIZE);
+    save_oper_backend(fd, least_used, sp, 0);
+    struct ident_t *ident = find_ident_by_reg(least_used);
+    if(ident != NULL) {
+        save_ident(fd, ident);
+    }
+    push_in_mapping_history(least_used, ident);
+    inc_idents_addresses();
+    return least_used;
+}
+
+void free_storage(FILE *fd, storage_t storage) {
+    load_oper_backend(fd, storage, sp, 0);
+    addi_oper_backend(fd, sp, sp, WORD_SIZE);
+    dec_idents_addresses();
+    pop_from_mapping_history(fd, storage);
+}
+
+void allocate_storage(storage_t storage) {
+    if(storage != zero)
+        storage_access[storage - r1] = 0;
+}
 
 struct ident_list_t *idents = NULL;
 struct ident_t *find_ident_by_name(char *name) {
@@ -80,11 +117,13 @@ int load_ident(FILE *fd, storage_t dest, char* name, bool create_if_not_exists) 
         if(!create_if_not_exists) {
             return -1;
         }
-        struct ident_t *new_ident = malloc(sizeof(struct ident_t));
-        new_ident->name = malloc(strlen(name)+1);
-        strcpy(new_ident->name, name);
-        new_ident->map = ON_REGISTER;
-        new_ident->addr.reg = dest;
+        struct ident_list_t *new_ident = malloc(sizeof(struct ident_list_t));
+        new_ident->ident.name = malloc(strlen(name)+1);
+        strcpy(new_ident->ident.name, name);
+        new_ident->ident.map = ON_REGISTER;
+        new_ident->ident.addr.reg = dest;
+        new_ident->next = (struct list_header_t *)idents;
+        idents = new_ident;
         return 0;
     }
     switch (ident->map) {
@@ -109,4 +148,22 @@ int load_ident(FILE *fd, storage_t dest, char* name, bool create_if_not_exists) 
         break;
     }
     return 0;
+}
+void save_ident(FILE *fd, struct ident_t *ident) {
+    ident->map = ON_STACK;
+    ident->addr.offset = 0;
+}
+void inc_idents_addresses() {
+    for (struct ident_list_t *cur = idents; cur != NULL; cur = (struct ident_list_t *)cur->next) {
+        if(cur->ident.map == ON_STACK) {
+            cur->ident.addr.offset += WORD_SIZE;
+        }
+    }
+}
+void dec_idents_addresses() {
+    for (struct ident_list_t *cur = idents; cur != NULL; cur = (struct ident_list_t *)cur->next) {
+        if(cur->ident.map == ON_STACK) {
+            cur->ident.addr.offset -= WORD_SIZE;
+        }
+    }
 }
